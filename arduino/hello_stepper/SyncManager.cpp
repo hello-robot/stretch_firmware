f2e6866e367ddf8b5c83ca2a8b95379269dee088
Aaron Edsinger <aedsinger@hello-robot.com>
2023-03-22 12:15:28 -0700

    fix sync code

--- arduino/hello_stepper/SyncManager.cpp
+++ arduino/hello_stepper/SyncManager.cpp
@@ -33,8 +33,6 @@ SyncManager::SyncManager()
   motor_sync_triggered=false;
   last_pulse_duration=0;
   runstop_trigger_cnt=0;
-  attachInterrupt(digitalPinToInterrupt(BOARD_VARIANT_PIN_RUNSTOP), handleIRQ, RISING);
-  irq_cnt=0;
 }
 
 
@@ -52,7 +50,6 @@ SyncManager::SyncManager()
 #define RUNSTOP_TRIGGER_MS 80
 
 
-
 void  SyncManager::step() //Called at 1Khz from TC4 loop
 {
 
@@ -68,21 +65,23 @@ void  SyncManager::step() //Called at 1Khz from TC4 loop
           runstop_active=1;
           runstop_trigger_cnt++;
           last_pulse_duration=RUNSTOP_TRIGGER_MS;
-        }
+        }    
       }
       else
       {
         if(rs_last) //falling ege
         {
           last_pulse_duration=pulse_count;
+          if(sync_mode_enabled && pulse_count>SYNC_PULSE_MIN_MS && pulse_count<SYNC_PULSE_MAX_MS)
+            motor_sync_triggered=true;
+          else
+            motor_sync_triggered=false;
         }
         runstop_active=0;
         pulse_count=0;
       }
     rs_last=rs;
    }
-
-
    if (BOARD_VARIANT==1 || BOARD_VARIANT==2)
    {
     //Poll line at 1Khz
@@ -110,24 +109,7 @@ void  SyncManager::step() //Called at 1Khz from TC4 loop
    }
 }
 
-void handleIRQ()
-{
-  sync_manager.irq_cnt++;
-  if (BOARD_VARIANT==0)
-  {
-      if(sync_manager.sync_mode_enabled)
-        sync_manager.motor_sync_triggered=true;
-}
-}
 
-void SyncManager::setupSyncManager() {
-  rs_last=digitalRead(BOARD_VARIANT_PIN_RUNSTOP);
-  if (BOARD_VARIANT==1 || BOARD_VARIANT==2)
-    sync_last=digitalRead(PIN_SYNC);
-
-  attachInterrupt(digitalPinToInterrupt(BOARD_VARIANT_PIN_RUNSTOP), handleIRQ, RISING);
-  irq_cnt=0;
-}
 
 void SyncManager::setupSyncManager() {  
   rs_last=digitalRead(BOARD_VARIANT_PIN_RUNSTOP);
